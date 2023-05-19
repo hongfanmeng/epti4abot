1886c3732413473a4a4c94b6ba7e9fdc9afd393e
pi <example@example.com>
2023-05-19 10:06:37 +0000

    chore: try to fix memory problem of imu

--- src/witimunode.cpp
+++ src/witimunode.cpp
@@ -12,11 +12,8 @@
 #include "nav_msgs/msg/odometry.hpp"
 
 #include "rclcpp/rclcpp.hpp"
-// #include "epti4abot/dfrobot.hpp"
 #include "epti4abot/serial.hpp"
-// #include "epti4abot/witimunode.hpp"
 
-// #include "JY901.h"
 #include "epti4abot/JY61P.h"
 
 using namespace std::chrono_literals;
@@ -26,16 +23,20 @@ class WitImuNode : public rclcpp::Node
 public:
   WitImuNode() : Node("wit_imu_node")
   {
-    int output_hz = 100;
-    timer_ms = std::chrono::milliseconds(1000 / output_hz);
-    frame_id = "wit_sensor";
+    std::string port;
+    int baudrate, update_rate;
 
     port = this->declare_parameter<std::string>("port", "/dev/ttyUSB0");
     baudrate = this->declare_parameter<int>("baudrate", 9600);
+    update_rate = this->declare_parameter<int>("update_rate", 100);  // 100 Hz
+    frame_id = this->declare_parameter<std::string>("frame_id", "wit_sensor");
+
     ser = new DF::Serial(port, baudrate);
 
-    RCLCPP_INFO(this->get_logger(), "Port: %s", port.c_str());
-    RCLCPP_INFO(this->get_logger(), "Baudrate: %d", baudrate);
+    RCLCPP_INFO(this->get_logger(), "port: %s", port.c_str());
+    RCLCPP_INFO(this->get_logger(), "frame_id: %s", frame_id.c_str());
+    RCLCPP_INFO(this->get_logger(), "update_rate: %dHz", update_rate);
+    RCLCPP_INFO(this->get_logger(), "baudrate: %d", baudrate);
 
     if (ser->isOpened())
     {
@@ -49,6 +50,8 @@ public:
 
     pub_imu = this->create_publisher<sensor_msgs::msg::Imu>("/imu", 20);
     imu = Wit::JY61P();
+
+    auto timer_ms = std::chrono::milliseconds(1000 / update_rate);
     timer_ = this->create_wall_timer(timer_ms, std::bind(&WitImuNode::timer_callback, this));
   }
 
@@ -60,65 +63,50 @@ public:
 private:
   void timer_callback()
   {
-    int length = 60;
-
-    std::string str = ser->recv(length);
+    std::string str = ser->recv(60);
     int count = str.size();
-    if (count != 0)
+    if (count == 0)
     {
-      rclcpp::Time now = this->get_clock()->now();
-      unsigned char* read_buf = new unsigned char[count];
-      if (str.size() > 0)
-      {
-        std::string tmp;
-        for (int i = 0; i < length; ++i)
-        {
-          read_buf[i] = str[i];
-          tmp.append(to_string(int(read_buf[i])).c_str()).append(" ");
-        }
-      }
-      imu.FetchData((char*)read_buf, count);
-      delete read_buf;
-
-      sensor_msgs::msg::Imu imu_data;
-
-      imu_data.header.stamp = now;
-      imu_data.header.frame_id = frame_id;
-
-      imu_data.linear_acceleration.x = imu.acc.x;
-      imu_data.linear_acceleration.y = imu.acc.y;
-      imu_data.linear_acceleration.z = imu.acc.z;
-
-      RCLCPP_INFO(this->get_logger(), "linear_acc, x=%f, y=%f, z=%f", imu.acc.x, imu.acc.y, imu.acc.z);
-
-      imu_data.angular_velocity.x = imu.gyro.x;
-      imu_data.angular_velocity.y = imu.gyro.y;
-      imu_data.angular_velocity.z = imu.gyro.z;
-
-      RCLCPP_INFO(this->get_logger(), "angular_vel: x=%f, y=%f, z=%f", imu.gyro.x, imu.gyro.y, imu.gyro.z);
-
-      tf2::Quaternion curr_quater;
-      curr_quater.setRPY(imu.angle.r, imu.angle.p, imu.angle.y);  // zyx
-
-      imu_data.orientation.x = curr_quater.x();
-      imu_data.orientation.y = curr_quater.y();
-      imu_data.orientation.z = curr_quater.z();
-      imu_data.orientation.w = curr_quater.w();
-
-      RCLCPP_INFO(this->get_logger(), "orientation: x=%f, y=%f, z=%f, w=%f", imu_data.orientation.x,
-                  imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w);
-
-      pub_imu->publish(imu_data);
+      return;
     }
+    imu.FetchData((char*)str.c_str(), count);
+
+    sensor_msgs::msg::Imu imu_data;
+
+    rclcpp::Time now = this->get_clock()->now();
+    imu_data.header.stamp = now;
+    imu_data.header.frame_id = frame_id;
+
+    imu_data.linear_acceleration.x = imu.acc.x;
+    imu_data.linear_acceleration.y = imu.acc.y;
+    imu_data.linear_acceleration.z = imu.acc.z;
+
+    imu_data.angular_velocity.x = imu.gyro.x;
+    imu_data.angular_velocity.y = imu.gyro.y;
+    imu_data.angular_velocity.z = imu.gyro.z;
+
+    tf2::Quaternion curr_quater;
+    curr_quater.setRPY(imu.angle.r, imu.angle.p, imu.angle.y);  // zyx
+
+    imu_data.orientation.x = curr_quater.x();
+    imu_data.orientation.y = curr_quater.y();
+    imu_data.orientation.z = curr_quater.z();
+    imu_data.orientation.w = curr_quater.w();
+
+    RCLCPP_INFO(this->get_logger(), "linear_acc:  x=%.4f, y=%.4f, z=%.4f", imu_data.linear_acceleration.x,
+                imu_data.linear_acceleration.y, imu_data.linear_acceleration.z);
+    RCLCPP_INFO(this->get_logger(), "angular_vel: x=%.4f, y=%.4f, z=%.4f", imu_data.angular_velocity.x,
+                imu_data.angular_velocity.y, imu_data.angular_velocity.z);
+    RCLCPP_INFO(this->get_logger(), "orientation: x=%.4f, y=%.4f, z=%.4f, w=%.4f", imu_data.orientation.x,
+                imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w);
+
+    pub_imu->publish(imu_data);
   }
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr pub_imu;
   Wit::JY61P imu;
-  std::string port;
-  int baudrate;
   DF::Serial* ser;
-  std::chrono::milliseconds timer_ms;
   std::string frame_id;
 };
 
